c9b6408a7798d6400a15fe167adc6183686a30b4
Abhisheknegii <abhibaba007@gmail.com>
2022-09-10 16:10:33 +0530

    using localStorage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import { Switch, Route } from 'react-router-dom';
-
+import { Switch, Route,Redirect } from 'react-router-dom';
+import { useContext } from 'react';
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import AuthContext from './Store/auth-context';
 
 function App() {
+  const authCtx=useContext(AuthContext)
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        {!authCtx.isLoggedin && <Route path='/auth'>
           <AuthPage />
+        </Route>}
+          <Route path='/profile'>
+          {authCtx.isLoggedin && <UserProfile />}
+          {!authCtx.isLoggedin && < Redirect to ='/auth'/>}
         </Route>
-        <Route path='/profile'>
-          <UserProfile />
+        <Route path='*'>
+          <Redirect to ='/'/>
         </Route>
       </Switch>
     </Layout>
